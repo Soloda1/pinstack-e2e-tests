83276b1fe173bf3bb3c192c0d4ce5b85287a1349
Soloda1 <enderderder@gmail.com>
2025-07-30 16:24:42 +0400

    feature: вынес TestMain и глобальные переменные в отдельный файл пакета

---
+++ internal/scenarios/integration/gateway_auth/gateway_auth_init_test.go
@@ -0,0 +1,37 @@
+package gateway_auth
+
+import (
+	"flag"
+	"github.com/Soloda1/pinstack-system-tests/config"
+	"github.com/Soloda1/pinstack-system-tests/internal/client"
+	"github.com/Soloda1/pinstack-system-tests/internal/logger"
+	"os"
+	"testing"
+)
+
+var (
+	cfg        *config.Config
+	log        *logger.Logger
+	apiClient  *client.Client
+	authClient *client.AuthClient
+)
+
+// TestMain запускается перед любым тестом в пакете и настраивает тестовое окружение
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	cfg = config.MustLoad()
+
+	log = logger.New(cfg.Env)
+	log.Info("Starting auth gateway tests", "env", cfg.Env)
+
+	apiClient = client.NewClient(cfg, log)
+	authClient = client.NewAuthClient(apiClient)
+
+	log.Info("Setup completed, starting tests")
+	code := m.Run()
+
+	log.Info("Tests finished")
+
+	os.Exit(code)
+}
